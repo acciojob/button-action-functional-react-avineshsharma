b0f85566309447176bb47339af8bd6bf926f72df
avineshsharma <jangraavinesh@gmail.com>
2026-01-05 17:16:03 +0530

    first solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,20 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isshow , setisshow] = useState(false)
+      
+  function handle(){
+    setisshow(true)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+     <button id = "click" onClick={handle}>click</button>
+    
+
+    {isshow && (
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy .</p>
+
+    )}
     </div>
   );
 }
